fd6602f53c91c12b6207ac5c68a7849f97d4f060
Solomon Broadbent <solomonbroadbentnz@gmail.com>
2023-11-16 01:36:37 +1100

    add script stuff for digital ocean domain name stuff

--- build-scripts/digital-ocean.sh
+++ build-scripts/digital-ocean.sh
@@ -18,3 +18,6 @@ doctl compute firewall create --name password.solthe.dev \
 	--inbound-rules "protocol:tcp,ports:80,address:0.0.0.0/0"
 
 doctl compute firewall add-droplets a7603bd2-1f23-44ce-a29f-2b14d5b6d9fa --droplet-ids 385124377
+
+doctl compute domain records create solthe.dev --record-type A --record-name password --record-data 170.64.158.163 --record-ttl 30
+
